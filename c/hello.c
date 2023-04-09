c252e1c8b1a510148657ae0d1a8be4dd70fa071e
Sadia Islam <sadiaislam@Sadias-MacBook-Air.local>
2023-04-09 23:00:30 +0600

    eof program

--- c/hello.c
+++ c/hello.c
@@ -1,6 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+   int num,v,even=0,pos=0,zero=0;
+   scanf("%d",&num);
+   for(int i= 0; i < num ; i ++) 
+   {
+       scanf("%d",&v);
+       if(v % 2 == 0) even++;
+       if(v > 0) pos++;
+       if(v == 0) zero++;
+
+   }
+      printf("Even: %d\n",even);
+      printf("Odd: %d\n",num - even);
+      printf("Positive: %d\n",pos);
+      printf("Negative: %d\n",num - (pos + zero));
+
     return 0;
-}
+} 
